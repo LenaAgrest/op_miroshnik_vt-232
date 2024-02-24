c9edbab04342346b681014f223a98ba35caa05a9
LenaAgrest <70846893+LenaAgrest@users.noreply.github.com>
2024-02-24 23:13:00 +0300

    create code

---
+++ libs/data_structures/vector/vector.h
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include <stdbool.h>
+
+typedef struct vector {
+int *data; // указатель на элементы вектора
+size_t size; // размер вектора
+size_t capacity; // вместимость вектора
+} vector;
+
+vector createVector(size_t n){
+    vector new;
+
+    if (n == 0){
+        new.data = NULL;
+    } else {
+        new.data = (int *) malloc(n * sizeof(int));
+        if (!new.data){
+            exceptBadAlloc();
+        }
+    }
+
+    new.size = 0;
+    new.capacity = n;
+
+    return new;
+}
