ab778b1b8ec4f4adcd8bf7876718ee2f90f07a37
lena agrest <lena.agrest@yandex.ru>
2024-02-26 21:25:15 +0300

    IT`S WORK!!!!!!!!!!!!!!

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -10,7 +10,8 @@ vector createVector(size_t n){
     } else {
         new.data = (int *) malloc(n * sizeof(int));
         if (!new.data){
-            exceptBadAlloc();
+            fprintf(stderr, "bad alloc");
+            exit(1);
         }
     }
 
@@ -20,33 +21,28 @@ vector createVector(size_t n){
     return new;
 }
 
-void exceptBadAlloc(){
-    fprintf(stderr, "bad alloc");
-    exit(1);
-}
-
-void reserve(vector *v, size_t newCapacity) {
-    if (newCapacity > v->capacity) {
-        int *newData = (int *) realloc(v->data, newCapacity * sizeof(int));
-        if (newData == NULL) {
-            fprintf(stderr, "Failed to reallocate memory for the vector\n");
-            exit(1);
-        }
-        v->data = newData;
-        v->capacity = newCapacity;
-    } else if (newCapacity == 0) {
+void reserve(vector *v, size_t newCapacity){
+    if (newCapacity == 0) {
         v->data = NULL;
-    } else if (newCapacity < v->size) {
+    } else {
+        v->data = (int *) realloc(v->data, sizeof(int) * newCapacity);
+    }
+    if (v->data == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+    if (v->size > newCapacity) {
         v->size = newCapacity;
     }
+    v->capacity = newCapacity;
 }
 
 void clear(vector *v) {
     v->size = 0;
 }
 
-void shrinkToFit(vector *v) {
-    int *new_data = (int *) realloc(v->data, v->size * sizeof(int));
+void shrinkToFit(vector *v){
+    reserve(v, v->size);
 }
 
 void deleteVector(vector *v) {
@@ -68,14 +64,12 @@ int getVectorValue(vector *v, size_t i) {
     return (v->data[i]);
 }
 
-void pushBack(vector *v, int x) {
-    if (v->capacity == 0) {
+void pushBack(vector *v, int x){
+    if (v->capacity == 0)
         reserve(v, 1);
-    } else if (v->capacity == v->size) {
-        reserve(v, v->capacity * 2);
-    }
-    v->data[v->size] = x;
-    v->size++;
+    else if (v->size == v->capacity)
+        reserve(v, v->size * 2);
+    v->data[v->size++] = x;
 }
 
 void popBack(vector *v) {
