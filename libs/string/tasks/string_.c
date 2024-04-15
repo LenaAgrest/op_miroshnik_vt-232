3e37c41c36b26e4e2abd9f2aacc835698cbd8c45
lena agrest <lena.agrest@yandex.ru>
2024-04-15 11:07:30 +0300

    end

--- libs/string/tasks/string_.c
+++ libs/string/tasks/string_.c
@@ -45,6 +45,53 @@ char *findSpaceReverse(char *rbegin, const char *rend) {
     return rbegin;
 }
 
+int strcmp_(const char *lhs, const char *rhs) {
+    while (*lhs == *rhs && *lhs != '\0' && *rhs != '\0') {
+        lhs++;
+        rhs++;
+    }
+    return *lhs - *rhs;
+}
+
+char* copy(const char *beginSource, const char *endSource, char *beginDestination) {
+    size_t size = endSource - beginSource;
+    memcpy(beginDestination, beginSource, size);
+    *(beginDestination + size) = '\0';
+
+    return beginDestination + size;
+}
+
+char* copyIf(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int)) {
+    while (beginSource != endSource) {
+        if (f( *beginSource)) {
+            *beginDestination = *beginSource;
+            beginDestination++;
+        }
+        beginSource++;
+    }
+    *beginDestination = '\0';
+
+    return beginDestination;
+}
+
+char* copyIfReverse(char *rbeginSource, const char *rendSource, char *beginDestination, int (*f)(int)) {
+    char *beginDestination_ = beginDestination;
+    while (rbeginSource >= rendSource) {
+        if (f(*rbeginSource)) {
+            *beginDestination_ = *rbeginSource;
+            beginDestination_++;
+        }
+        rbeginSource--;
+    }
+    *beginDestination_ = '\0';
+
+    return beginDestination_;
+}
+
+int is_h(char s) {
+    return s == 'h' || s == 'w';
+}
+
 void test_find() {
     char string[] = "lab_17";
 
@@ -75,6 +122,14 @@ void test_find_space_reverse() {
     assert(findSpaceReverse(string + 3, string) == string - 1);
 }
 
+void test_strcmp_(){
+    char string1[] = "lab_17";
+    char string2[] = "laba_17";
+
+    assert(strcmp_(string1, string2) < 0);
+}
+
+
 void test() {
 
 test_find();
@@ -82,5 +137,6 @@ test_find_non_space();
 test_find_space();
 test_find_non_space_reverse();
 test_find_space_reverse();
+test_strcmp_();
 
 }
