0b717224a887332f13f4c874121e6b5576053448
lena agrest <lena.agrest@yandex.ru>
2024-05-28 21:57:39 +0300

    task 3 and 4

--- libs/files/task.c
+++ libs/files/task.c
@@ -58,9 +58,7 @@ void generateFileWithRandomSquareMatrices(const char* filename, int numMatrices,
     fclose(file);
 }
 
-//1. В текстовом файле хранятся целочисленные квадратные матрицы следующим образом:
-// сначала целое число n – порядок матрицы, а затем ее элементы по строкам.
-// Преобразовать файл так, чтобы элементы матрицы хранились по столбцам.
+//1
 int task_1(const char *str1) {
     FILE *inputFile, *outputFile;
     int n, i, j;
@@ -104,8 +102,7 @@ int task_1(const char *str1) {
     return 0;
 }
 
-//2. В текстовом файле записаны вещественные числа в форме с фиксированной точкой. Преобразовать файл,
-// представив каждое число в форме с плавающей точкой, сохранив две цифры после точки в мантиссе
+//2
 int task_2(const char *str) {
     FILE *inputFile, *outputFile;
     float number;
@@ -124,6 +121,226 @@ int task_2(const char *str) {
 
     fclose(inputFile);
     fclose(outputFile);
+
+    return 0;
+}
+
+//заполняет файл случайным арифметическим выражением
+void generateFileWithRandomMathExpression(const char* filename) {
+    FILE *file = fopen(filename, "w");
+    int num1, num2, num3;
+    char operator, operator1;
+
+    if (file == NULL) {
+        printf("Error open file 4\n");
+        return;
+    }
+
+    srand(time(NULL));
+
+    num1 = rand() % 10;
+    num2 = rand() % 10;
+    operator = rand() % 4; // 0 - '+', 1 - '-', 2 - '*', 3 - '/'
+
+    fprintf(file, "%d %c %d", num1, operator == 0 ? '+' : operator == 1 ? '-' : operator == 2 ? '*' : '/', num2);
+
+    if (num1 < 5){
+        num3 = rand() % 10;
+        operator1 = rand() % 4;
+        fprintf(file, " %c %d", operator1 == 0 ? '+' : operator1 == 1 ? '-' : operator1 == 2 ? '*' : '/', num3);
+    }
+
+    fclose(file);
+}
+
+//3
+int task_3(const char *str) {
+    FILE *file, *file1;
+    char expression[100];
+    int result;
+    char ch;
+    int count = 0;
+
+    file = fopen("C:\\Users\\wwwri\\files\\original_task_3.txt", "r");
+    if (file == NULL) {
+        printf("Error open file 5\n");
+        return 1;
+    }
+    while ((ch = fgetc(file)) != EOF) {
+        count++;
+    }
+    fclose(file);
+
+    file1 = fopen("C:\\Users\\wwwri\\files\\original_task_3.txt", "r+");
+    int num1, num2, num3;
+    char op1, op2;
+    if (count == 5){
+        fscanf(file1, "%d %c %d", &num1, &op1, &num2);
+
+        if (op1 == '+') {
+            result = num1 + num2;
+        } else if (op1 == '-') {
+            result = num1 - num2;
+        } else if (op1 == '*') {
+            result = num1 * num2;
+        } else if (op1 == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                printf("Error: division by zero\n");
+                fclose(file1);
+                return 1;
+            }
+        } else {
+            printf("Error: invalid operator 1\n");
+            fclose(file1);
+            return 1;
+        }
+    } else if (count == 9){
+        fscanf(file1, "%d %c %d %c %d", &num1, &op1, &num2, &op2, &num3);
+
+        if (op1 == '+' && op2 == '+') {
+            result = num1 + num2 + num3;
+        } else if (op1 == '+' && op2 == '-') {
+            result = num1 + num2 - num3;
+        } else if (op1 == '+' && op2 == '*') {
+            result = num1 + (num2 * num3);
+        } else if (op1 == '+' && op2 == '/') {
+            if (num3 != 0) {
+                result = num1 + (num2 / num3);
+            } else {
+                fprintf(file1, "\nError: division by zero");;
+                fclose(file1);
+                return 1;
+            }
+        } else if (op1 == '-' && op2 == '+') {
+            result = num1 - num2 + num3;
+        } else if (op1 == '-' && op2 == '-') {
+            result = num1 - num2 - num3;
+        } else if (op1 == '-' && op2 == '*') {
+            result = num1 - (num2 * num3);
+        } else if (op1 == '-' && op2 == '/') {
+            if (num3 != 0) {
+                result = num1 - (num2 / num3);
+            } else {
+                fprintf(file1, "\nError: division by zero");;
+                fclose(file1);
+                return 1;
+            }
+        } else if (op1 == '*' && op2 == '+') {
+            result = num1 * num2 + num3;
+        } else if (op1 == '*' && op2 == '-') {
+            result = num1 * num2 - num3;
+        } else if (op1 == '*' && op2 == '*') {
+            result = num1 * num2 * num3;
+        } else if (op1 == '*' && op2 == '/') {
+            if (num3 != 0) {
+                result = num1 * num2 / num3;
+            } else {
+                fprintf(file1, "\nError: division by zero");;
+                fclose(file1);
+                return 1;
+            }
+        } else if (op1 == '/' && op2 == '+') {
+            if (num2 != 0) {
+                result = num1 / num2 + num3;
+            } else {
+                fprintf(file1, "\nError: division by zero");;
+                fclose(file1);
+                return 1;
+            }
+        } else if (op1 == '/' && op2 == '-') {
+            if (num2 != 0) {
+                result = num1 / num2 - num3;
+            } else {
+                fprintf(file1, "\nError: division by zero");;
+                fclose(file1);
+                return 1;
+            }
+        } else if (op1 == '/' && op2 == '*') {
+            if (num2 != 0) {
+                result = num1 / num2 * num3;
+            } else {
+                fprintf(file1, "\nError: division by zero");;
+                fclose(file1);
+                return 1;
+            }
+        } else if (op1 == '/' && op2 == '/') {
+            if (num2 != 0 && num3 != 0) {
+                result = num1 / num2 / num3;
+            } else {
+                fprintf(file1, "\nError: division by zero");;
+                fclose(file1);
+                return 1;
+            }
+        } else {
+            printf("Error: invalid operator2\n");
+            fclose(file1);
+            return 1;
+        }
+    } else {
+        fclose(file1);
+    }
+
+    fprintf(file1, "\nResult: %d\n", result);
+
+    fclose(file1);
+
+    return 0;
+}
+
+//заполняет файл различными последовательностями символов через пробел
+void generateFileWithDifSequences(const char* filename, int numWords) {
+    FILE *file = fopen(filename, "w");
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+    int i, j;
+
+    if (file == NULL) {
+        printf("Ошибка открытия файла\n");
+        return;
+    }
+
+    srand(time(NULL));
+
+    for (i = 0; i < numWords; i++) {
+        char word[rand() % 100];
+        for (j = 0; j < rand() % 100; j++) {
+            word[j] = alphabet[rand() % strlen(alphabet)]; //выбор случайной буквы из алфавита
+        }
+        word[rand() % 100] = '\0';
+        fprintf(file, "%s ", word);
+    }
+
+    fclose(file);
+}
+
+//Задание 4: сохранить в файле только те слова, которые содержат данную последовательность символов
+int task_4(const char* sourceFilename, const char* destinationFilename, const char* sequence) {
+    FILE *sourceFile, *destinationFile;
+
+    sourceFile = fopen(sourceFilename, "r");
+    if (sourceFile == NULL) {
+        printf("Ошибка открытия исходного файла\n");
+        return 1;
+    }
+
+    destinationFile = fopen(destinationFilename, "w");
+    if (destinationFile == NULL) {
+        printf("Ошибка открытия файла назначения\n");
+        fclose(sourceFile);
+        return 1;
+    }
+
+    char word[MAX_FILE_SIZE];
+
+    while (fscanf(sourceFile, "%s", word) == 1) {
+        if (strstr(word, sequence) != NULL) {
+            fprintf(destinationFile, "%s\n", word);
+        }
+    }
+
+    fclose(sourceFile);
+    fclose(destinationFile);
 
     return 0;
 }
--- libs/files/test.c
+++ libs/files/test.c
@@ -26,9 +26,34 @@ void test_task_2() {
 
     }
 }
+
+void test_task_3() {
+    const char *str1 = "C:\\Users\\wwwri\\files\\converted_task_3.txt";
+    const char *str2 = "C:\\Users\\wwwri\\files\\for_verification_task_3.txt";
+    int r = task_3(str1);
+    printf("3:");
+
+    if (r == 0){
+        assertTXT(str1, str2);
+
+    }
+}
+
+void test_task_4() {
+    const char *str1 = "C:\\Users\\wwwri\\files\\original_task_4.txt";
+    const char *str2 = "C:\\Users\\wwwri\\files\\converted_task_4.txt";
+    const char *str3 = "C:\\Users\\wwwri\\files\\for_verification_task_4.txt";
+    int r = task_4(str1, str2, "cu");
+    printf("4:");
+    if (r == 0){
+        assertTXT(str2, str3);
+    }
+}
 int main(){
     SetConsoleOutputCP(CP_UTF8);
 
     test_task_1();
     test_task_2();
+    test_task_3();
+    test_task_4();
 }
